8968b533a15d7abb14c343cc980ff2c3e23b8141
Raúl Heredia <raul.heredia01@outlook.com>
2022-05-17 16:14:03 +0200

    Ahora si acabado casi me da un ataque al corazon jiji

--- src/views/lista/Lista.jsx
+++ src/views/lista/Lista.jsx
@@ -2,11 +2,13 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import { calcCrow } from '../../assets/functions/calcDistancia'
-
+import { resetClasses, addClass, switchClasses } from "./classes";
 import "./Lista.css";
+
 export default function Lista() {
     const [gasolineras, setGasolineras] = useState([]);
     const [gasFilter, setGasFilter] = useState([]);
+    const [gasFilterSearch, setGasFilterSearch] = useState(['']);
     const [distanciaFilter, setDistanciaFilter] = useState(5);
     const [mediasCombustible, setMediasCombustible] = useState({});
     const [ubicacionUsuari, setUbicacionUsuari] = useState({})
@@ -15,9 +17,19 @@ export default function Lista() {
     useEffect(() => {
         document.body.classList.remove('overflownt');
         document.body.classList.add('overflow');
-        let gasTemp = JSON.parse(sessionStorage.getItem('gasolineras'));
+        let gasTemp = JSON.parse(sessionStorage.getItem('gasolineras')) || 0;
+        if (!gasTemp) {
+            console.log("if");
+            window.location.href = window.location.protocol + "//" + window.location.host;
+        }
         let ubiTemp = JSON.parse(sessionStorage.getItem('ubicacionUsuari'));
-        if (!gasTemp) window.location.href = window.location.protocol + "//" + window.location.host;
+        if (!ubiTemp) {
+            ubiTemp = {
+                lat: 40.416775,
+                lng: -3.703790
+            };
+        }
+        console.log("splice")
         gasTemp.splice(0, 1);
         gasTemp.splice(0, 1);
         setUbicacionUsuari(ubiTemp);
@@ -54,6 +66,7 @@ export default function Lista() {
         })
         media95 = (media95 / c95).toFixed(3); media98 = (media98 / c98).toFixed(3); mediaGasoilA = (mediaGasoilA / cGasA).toFixed(3); mediaGasoilP = (mediaGasoilP / cGasP).toFixed(3); mediaGnc = (mediaGnc / cGnc).toFixed(3); mediaGnl = (mediaGnl / cGnl).toFixed(3); mediaGlp = (mediaGlp / cGlp).toFixed(3);
         mediasTemp.media95 = media95; mediasTemp.media98 = media98; mediasTemp.mediaGasA = mediaGasoilA; mediasTemp.mediaGasP = mediaGasoilP; mediasTemp.mediaGnc = mediaGnc; mediasTemp.mediaGnl = mediaGnl; mediasTemp.mediaGlp = mediaGlp;
+
         gasFilterTemp.sort((a, b) => {
             if (orderBy) {
                 if (!a[sortBy]) a[sortBy] = 999;
@@ -67,47 +80,64 @@ export default function Lista() {
         setMediasCombustible(mediasTemp);
     }
     function handleClick(ev) {
-        setSortBy(ev.target.id)
+        if (ev.target.id == "rotulo") return;
+        let cssClass = orderBy ? 'order-asc' : 'order-desc';
+        resetClasses();
+        setSortBy(ev.target.id);
+        addClass(ev.target.id, cssClass);
     }
     function handleDistance(ev) {
-        setDistanciaFilter(ev.target.value * 1)
+        setDistanciaFilter(ev.target.value * 1);
     }
     function handleOrderBy(ev) {
-        setOrderBy(ev.target.value * 1)
+        setOrderBy(ev.target.value * 1);
+        switchClasses(ev.target.value * 1);
+    }
+    function handleSearch(ev) {
+        console.log(ev.target.value);
+        setGasFilterSearch(ev.target.value);
     }
     return (
         <main className="container mx-auto mb-2 px-4 pt-20">
             <div className="flex justify-center gap-3 flex-wrap">
                 <div className="mb-3 xl:w-96">
                     <label htmlFor="orderBy">Ordenar por</label>
-                    <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" name="orderBy" onChange={handleOrderBy}>
+                    <select className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" name="orderBy" onChange={handleOrderBy}>
                         <option value="1">Orden Ascendente</option>
                         <option value="0">Orden Descendente</option>
                     </select>
                 </div>
+                <div className="flex justify-center">
+                    <div className="mb-3 xl:w-96">
+                        <div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
+                            <label htmlFor="buscarGasolinera">Buscar Gasolinera</label>
+                            <input type="search" name="buscarGasolinera" onKeyUp={handleSearch} class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Buscar" aria-label="Search" aria-describedby="button-addon2" />
+                        </div>
+                    </div>
+                </div>
                 <div className="relative pt-1">
                     <label htmlFor="distance">Limitar Distancia a: <span className="text-gray-800">{distanciaFilter} KM</span></label>
-                    <input type="range" min="1" max="25" name="distance" value={distanciaFilter} className="w-full h-2 bg-gray-300 appearance-none" onChange={handleDistance} />
+                    <input type="range" min="1" max="25" name="distance" value={distanciaFilter} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" onChange={handleDistance} />
                 </div>
             </div>
 
             <div className="tablaCombustible rounded-lg border shadow-2xl mt-5">
                 <table className="w-full">
                     <thead>
-                        <tr className="bg-gray-800" onClick={handleClick}>
-                            <th className="p-4 text-neutral-50 text-left" id="distanciaA">Distancia</th>
-                            <th className="p-4 text-neutral-50 text-left" id="rotulo">Gasolinera</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gasolina95">Gasolina 95</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gasolina98" >Gasolina 98</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gasoilA" >Gasoil A</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gasoilPremium" >Gasoil +</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gnc" >GNC</th>
-                            <th className="p-4 text-neutral-50 text-left" id="gnl" >GNL</th>
-                            <th className="p-4 text-neutral-50 text-left" id="glp" >GLP</th>
+                        <tr className="bg-gray-800 ph-5" onClick={handleClick}>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer order-asc" id="distanciaA">Distancia</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-default" id="rotulo">Gasolinera</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gasolina95">Gasolina 95</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gasolina98" >Gasolina 98</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gasoilA" >Gasoil A</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gasoilPremium" >Gasoil +</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gnc" >GNC</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="gnl" >GNL</th>
+                            <th className="p-4 text-neutral-50 text-left cursor-pointer" id="glp" >GLP</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {gasFilter.map((gasolinera) => {
+                        {gasFilterSearch == '' ? gasFilter.map((gasolinera) => {
                             let nd = <span className='nd'>N/D</span>
                             return (
                                 <tr key={gasolinera.key} >
@@ -122,6 +152,23 @@ export default function Lista() {
                                     <td className={"p-4 " + (gasolinera.glp > mediasCombustible.mediaGlp ? 'caro' : 'barato')}>{gasolinera.glp && gasolinera.glp != 999 ? gasolinera.glp + "€" : nd}</td>
                                 </tr>
                             )
+                        }) : gasFilter.map((gasolinera) => {
+                            let nd = <span className='nd'>N/D</span>
+                            if (gasolinera.rotulo.startsWith(gasFilterSearch.toUpperCase())) {
+                                return (
+                                    <tr key={gasolinera.key} >
+                                        <td className="p-4">{gasolinera.distanciaA} KM</td>
+                                        <td className="p-4"><a className="text-blue-600 hover:text-blue-900 visited:text-blue-600" target="_blank" href={`https://maps.google.com/?ll=${gasolinera.latitud},${gasolinera.longitud}`}>{gasolinera.rotulo}</a></td>
+                                        <td className={"p-4 " + (gasolinera.gasolina95 > mediasCombustible.media95 ? 'caro' : 'barato')}>{gasolinera.gasolina95 && gasolinera.gasolina95 != 999 ? gasolinera.gasolina95 + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.gasolina98 > mediasCombustible.media98 ? 'caro' : 'barato')}>{gasolinera.gasolina98 && gasolinera.gasolina98 != 999 ? gasolinera.gasolina98 + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.gasoilA > mediasCombustible.mediaGasA ? 'caro' : 'barato')}>{gasolinera.gasoilA && gasolinera.gasoilA != 999 ? gasolinera.gasoilA + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.gasoilPremium > mediasCombustible.mediaGasP ? 'caro' : 'barato')}>{gasolinera.gasoilPremium && gasolinera.gasoilPremium != 999 ? gasolinera.gasoilPremium + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.gnc > mediasCombustible.mediaGnc ? 'caro' : 'barato')}>{gasolinera.gnc && gasolinera.gnc != 999 ? gasolinera.gnc + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.gnl > mediasCombustible.mediaGnl ? 'caro' : 'barato')}>{gasolinera.gnl && gasolinera.gnl != 999 ? gasolinera.gnl + "€" : nd}</td>
+                                        <td className={"p-4 " + (gasolinera.glp > mediasCombustible.mediaGlp ? 'caro' : 'barato')}>{gasolinera.glp && gasolinera.glp != 999 ? gasolinera.glp + "€" : nd}</td>
+                                    </tr>
+                                )
+                            }
                         })}
                     </tbody>
                 </table>
---
+++ src/views/lista/classes.js
@@ -0,0 +1,29 @@
+export function resetClasses() {
+    document.getElementById("distanciaA").classList.remove('order-asc', 'order-desc');
+    document.getElementById("rotulo").classList.remove('order-asc', 'order-desc')
+    document.getElementById("gasolina95").classList.remove('order-asc', 'order-desc');
+    document.getElementById("gasolina98").classList.remove('order-asc', 'order-desc');
+    document.getElementById("gasoilA").classList.remove('order-asc', 'order-desc');
+    document.getElementById("gasoilPremium").classList.remove('order-asc', 'order-desc');
+    document.getElementById("gnc").classList.remove('order-asc', 'order-desc');
+    document.getElementById("gnl").classList.remove('order-asc', 'order-desc');
+    document.getElementById("glp").classList.remove('order-asc', 'order-desc');
+}
+export function addClass(id, order) {
+    document.getElementById(id).classList.add(order)
+}
+
+export function switchClasses(order) {
+    let id;
+    if (document.getElementById("distanciaA").classList.contains('order-asc') || document.getElementById("distanciaA").classList.contains('order-desc')) id = "distanciaA";
+    if (document.getElementById("rotulo").classList.contains('order-asc') || document.getElementById("rotulo").classList.contains('order-desc')) id = "rotulo";
+    if (document.getElementById("gasolina95").classList.contains('order-asc') || document.getElementById("gasolina95").classList.contains('order-desc')) id = "gasolina95";
+    if (document.getElementById("gasolina98").classList.contains('order-asc') || document.getElementById("gasolina98").classList.contains('order-desc')) id = "gasolina98";
+    if (document.getElementById("gasoilA").classList.contains('order-asc') || document.getElementById("gasoilA").classList.contains('order-desc')) id = "gasoilA";
+    if (document.getElementById("gasoilPremium").classList.contains('order-asc') || document.getElementById("gasoilPremium").classList.contains('order-desc')) id = "gasoilPremium";
+    if (document.getElementById("gnc").classList.contains('order-asc') || document.getElementById("gnc").classList.contains('order-desc')) id = "gnc";
+    if (document.getElementById("gnl").classList.contains('order-asc') || document.getElementById("gnl").classList.contains('order-desc')) id = "gnl";
+    if (document.getElementById("glp").classList.contains('order-asc') || document.getElementById("glp").classList.contains('order-desc')) id = "glp";
+    resetClasses();
+    addClass(id, order ? 'order-asc' : 'order-desc')
+}
